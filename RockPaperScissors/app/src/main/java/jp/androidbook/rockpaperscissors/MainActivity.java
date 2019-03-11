c9a9748cb990d5aa9450208d16ac833687f72862
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2019-03-12 00:39:11 +0900

    メインシーンのレイアウト作成中
    ・じゃんけんボタン、テキストを仮配置しました（今後要調整）

--- RockPaperScissors/app/src/main/java/jp/androidbook/rockpaperscissors/MainActivity.java
+++ RockPaperScissors/app/src/main/java/jp/androidbook/rockpaperscissors/MainActivity.java
@@ -3,6 +3,7 @@ package jp.androidbook.rockpaperscissors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//==メインシーンのアクティビティー
 public class MainActivity extends AppCompatActivity {
 
     @Override
