0958a7d29638ca35933dee9e5fd5786200c168f1
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2019-03-17 04:36:38 +0900

    タイトル画面の作成
    ・タイトル画面を作成しました
    ・スタートボタンを押したらメインアクティビティが起動し、じゃんけんが始まるようにしました

---
+++ RockPaperScissors/app/src/main/java/jp/androidbook/rockpaperscissors/TitleActivity.java
@@ -0,0 +1,22 @@
+package jp.androidbook.rockpaperscissors;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+
+    //public関数==============================================
+    public void StartButtonClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    //========================================================
+}
